25a0ea30ed60d6f20ca484821a4730f9f8b26cf4
kevinroku <109759121+kevinroku@users.noreply.github.com>
2026-01-29 12:19:55 +0100

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -115,6 +115,9 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
+  disclaimer: `
+    Test.
+  `,
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
   `,
