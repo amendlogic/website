7be08b17b3683ded16ac9ebfd5d07799f4bae8b2
kevinroku <109759121+kevinroku@users.noreply.github.com>
2026-01-30 12:00:35 +0100

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -110,6 +110,7 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'TikTok', icon: 'tabler:brand-tiktok', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
