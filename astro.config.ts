8e2906f642c091568f00320329a3e32c6b38030b
kevinroku <109759121+kevinroku@users.noreply.github.com>
2026-02-05 12:08:52 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     locales: ["de", "en"],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: false,
+      redirectToDefaultLocale: true,
     }
   },
   
