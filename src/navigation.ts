4672efb6bf86dd8d8a54dd37645bb8a959a9836a
kewinrok <109759121+kewinrok@users.noreply.github.com>
2026-02-13 14:28:08 +0100

    Update navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -124,7 +124,7 @@ export const getFooterData = (lang = 'en') => {
       { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
     ],
     footNote: `
-    © ${new Date().getFullYear()} <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo">{SITE?.name}</a> · ${t('nav.rights')} · <a href="#" data-cc="show-preferencesModal" class="text-blue-600 underline dark:text-muted">Cookie Settings</a>
+    © ${new Date().getFullYear()} <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo">${SITE?.name}</a> · ${t('nav.rights')} · <a href="#" data-cc="show-preferencesModal" class="text-blue-600 underline dark:text-muted">Cookie Settings</a>
   `,
   };
 };
