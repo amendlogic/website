0380078cc77ba394a5c9353605ceb49ba5874496
kewinrok <109759121+kewinrok@users.noreply.github.com>
2026-02-13 12:19:13 +0100

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,22 +1,18 @@
 // Englisch Imports
 import navEn from './locales/en/nav.json';
 import homeEn from './locales/en/home.json';
-import footerEn from './locales/en/footer.json';
 
 // Deutsch Imports
 import navDe from './locales/de/nav.json';
 import homeDe from './locales/de/home.json';
-import footerDe from './locales/de/footer.json';
 
 export const ui = {
   en: {
     nav: navEn,
     home: homeEn,
-    footer: footerEn,
   },
   de: {
     nav: navDe,
     home: homeDe,
-    footer: footerDe,
   },
 } as const;
