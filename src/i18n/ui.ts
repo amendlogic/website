1c8024f736f64a2622bbbb401d34cf371d48e75d
kewinrok <109759121+kewinrok@users.noreply.github.com>
2026-02-19 08:18:51 +0100

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,14 +3,14 @@ import navEn from './locales/en/nav.json';
 import homeEn from './locales/en/home.json';
 import footerEn from './locales/en/footer.json';
 import fallbacksEn from './locales/en/fallbacks.json';
-import faqEn from './locales/en/help-center.json';
+import faqEn from './locales/en/help.json';
 
 // Deutsch Imports
 import navDe from './locales/de/nav.json';
 import homeDe from './locales/de/home.json';
 import footerDe from './locales/de/footer.json';
 import fallbacksDe from './locales/de/fallbacks.json';
-import faqDe from './locales/de/help-center.json';
+import faqDe from './locales/de/help.json';
 
 export const ui = {
   en: {
