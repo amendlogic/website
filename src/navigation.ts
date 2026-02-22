1677e91cebaa432be850a9d67969043f1742673a
kewinrok <109759121+kewinrok@users.noreply.github.com>
2026-02-22 11:53:19 +0100

    navigation.ts aktualisieren

--- src/navigation.ts
+++ src/navigation.ts
@@ -116,7 +116,7 @@ export const getFooterData = (lang = 'en', footnotes: string[] = []) => {
         href: getHomePermalink('de'), 
       },
     ],
-    footnotes.length > 0 ? footnotes : [
+    footnotes: footnotes.length > 0 ? footnotes : [
       'risk'
     ],
     socialLinks: [
