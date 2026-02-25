32f90b9ff8fd2ef8376b3aee4cdb51d4876b2afa
kewinrok <109759121+kewinrok@users.noreply.github.com>
2026-02-25 10:31:32 +0100

    Update ui.ts

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -3,6 +3,7 @@ import navEn from './locales/en/ui/nav.json';
 import homeEn from './locales/en/home.json';
 import aboutEn from './locales/en/about.json';
 import pricingEn from './locales/en/pricing.json';
+import performanceEn from './locales/en/performance.json';
 import footnotesEn from './locales/en/ui/footnotes.json';
 import fallbacksEn from './locales/en/ui/fallbacks.json';
 import faqEn from './locales/en/faq.json';
@@ -12,6 +13,7 @@ import navDe from './locales/de/ui/nav.json';
 import homeDe from './locales/de/home.json';
 import aboutDe from './locales/de/about.json';
 import pricingDe from './locales/de/pricing.json';
+import performanceDe from './locales/de/performance.json';
 import footnotesDe from './locales/de/ui/footnotes.json';
 import fallbacksDe from './locales/de/ui/fallbacks.json';
 import faqDe from './locales/de/faq.json';
@@ -22,6 +24,7 @@ export const ui = {
     home: homeEn,
     about: aboutEn,
     pricing: pricingEn,
+    performance: performanceEn,
     footnotes: footnotesEn,
     fallbacks: fallbacksEn,
     faq: faqEn,
@@ -31,6 +34,7 @@ export const ui = {
     home: homeDe,
     about: aboutDe,
     pricing: pricingDe,
+    performance: performanceDe,
     footnotes: footnotesDe,
     fallbacks: fallbacksDe,
     faq: faqDe,
