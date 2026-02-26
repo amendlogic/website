7fd57140aba316722782d7cafafd74cd5e37bebf
kewinrok <109759121+kewinrok@users.noreply.github.com>
2026-02-26 10:39:57 +0100

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -16,7 +16,7 @@ import astrowind from './vendor/integration';
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
 
 // import cloudflare from '@astrojs/cloudflare';
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -25,8 +25,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  output: 'static',
-  // adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
 
 i18n: {
     defaultLocale: "en",
